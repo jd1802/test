2bd3b0c4c7fe0b987b58fa342fbefc0c40695af2
Jarret Logan <jarret.logan@publicissapient.com>
2020-01-16 14:05:29 -0600

    Updates

---
+++ precache-manifest.ec92e10bd2abbbf89a30215d0cf7e4bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71ca5d6932b99ee2e09aa3e880ebb0e6",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "beba3fecf89cabcf6413",
+    "url": "/test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "53ce5d0dc126a28c432b",
+    "url": "/test/static/js/2.ce5eda19.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/test/static/js/2.ce5eda19.chunk.js.LICENSE"
+  },
+  {
+    "revision": "beba3fecf89cabcf6413",
+    "url": "/test/static/js/main.422ff989.chunk.js"
+  },
+  {
+    "revision": "f5855940c06bc3d17424",
+    "url": "/test/static/js/runtime-main.892c12c7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
